419b187af04b732d5a3520336b0958c9bb0f06f4
Aris Karamat <karamataris@gmail.com>
2025-10-07 15:38:56 +0200

    delete firebase api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http'; // ✅ importiere HttpClient provider
-import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environment/environment';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()), provideFirebaseApp(() => 
-      initializeApp({ 
-        projectId: "zum-schwarzen-ross", 
-        appId: "1:853990112853:web:3fd687aa2b225c2c959edc", 
-        storageBucket: "zum-schwarzen-ross.firebasestorage.app", 
-        apiKey: "AIzaSyBG3p9i3w1aBOAirfVMUuGX5goHMYu2nAA", 
-        authDomain: "zum-schwarzen-ross.firebaseapp.com", 
-        messagingSenderId: "853990112853", 
-})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+    provideHttpClient(withFetch()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
