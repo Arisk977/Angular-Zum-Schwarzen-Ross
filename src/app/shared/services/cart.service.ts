af34065aecca878d789a628788cb14ca74f5ccd5
Aris Karamat <karamataris@gmail.com>
2025-10-08 16:43:42 +0200

    created cart service

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface CartItem {
+  name: string;
+  description?: string;
+  price: number;
+  quantity: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  private items: CartItem[] = [];
+  private cartSubject = new BehaviorSubject<CartItem[]>([]);
+  cart$ = this.cartSubject.asObservable();
 
-  constructor() { }
+  addToCart(item: CartItem) {
+    const existing = this.items.find(i => i.name === item.name);
+    if (existing) {
+      existing.quantity++;
+    } else {
+      this.items.push({ ...item, quantity: 1 });
+    }
+    this.cartSubject.next(this.items);
+  }
+
+  removeFromCart(name: string) {
+    this.items = this.items.filter(i => i.name !== name);
+    this.cartSubject.next(this.items);
+  }
+
+  clearCart() {
+    this.items = [];
+    this.cartSubject.next(this.items);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  getTotal() {
+    return this.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  }
 }
