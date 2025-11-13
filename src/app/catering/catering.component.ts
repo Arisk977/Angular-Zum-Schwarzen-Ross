15e673484c349bb39e2b842b0fa1329b9ab6ed12
Aris Karamat <karamataris@gmail.com>
2025-11-13 16:53:06 +0100

    changed landingpage design

--- src/app/catering/catering.component.ts
+++ src/app/catering/catering.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../shared/component/button/button.component';
 
 @Component({
@@ -8,5 +8,19 @@ import { ButtonComponent } from '../shared/component/button/button.component';
   styleUrl: './catering.component.scss'
 })
 export class CateringComponent {
+@ViewChild('backgroundVideo') video!: ElementRef<HTMLVideoElement>;
 
+    ngAfterViewInit(): void {
+    const vid = this.video.nativeElement;
+    
+    vid.muted = true; // sicherstellen, dass gemutet ist
+    vid.play().catch(() => {
+      console.log('Autoplay blockiert, warte auf Klick');
+      const startVideo = () => {
+        vid.play();
+        document.removeEventListener('click', startVideo);
+      };
+      document.addEventListener('click', startVideo);
+    });
+  }
 }
